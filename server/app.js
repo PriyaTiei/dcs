d4d7c154323858b83dc65a9064249d84fc36c25c
Melwin Sam <melwin280@gmail.com>
2023-03-18 12:10:31 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const dcsFormRoutes = require("./routes/dcs-form");
+const cors = require("cors");
 const app = express();
 
 mongoose.set("strictQuery", true);
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.static("public"));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
