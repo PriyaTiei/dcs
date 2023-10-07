be00ee7803a7e46f8b5b17aef9b8b150db15e557
MOHAMMED YOUSUF <yousuf@tiei.toyota-industries.com>
2023-10-07 14:33:11 +0530

    reusable_A_leak data not eql to null

--- client/src/components/engno/processDetails/Reusable_A_LeakTest.js
+++ client/src/components/engno/processDetails/Reusable_A_LeakTest.js
@@ -9,8 +9,8 @@ function Reusable_A_LeakTest({ data, date, engineNo, dispatchedDate}) {
 display=     <div className="d-flex flex-wrap gap-0 deta ">
   <div className="text-center font-weight-bold flex-1">{engineNo}</div>
          
-<div className="text-center font-weight-bold flex-1">{data.slice(75,84)}</div>
-<div className="text-center font-weight-bold flex-1">{data.slice(84,85)=="2" ? "OK" : (data.slice(84,85)=="1" ? "NG" : "")}</div>
+<div className="text-center font-weight-bold flex-1">{data?.slice(75,84)}</div>
+<div className="text-center font-weight-bold flex-1">{data?.slice(84,85)=="2" ? "OK" : (data?.slice(84,85)=="1" ? "NG" : "")}</div>
 <div className="text-center font-weight-bold flex-1">{moment(date).format("DD-MM-YYYY HH:mm:ss")}</div>
 
 {/* <div className="text-center font-weight-bold flex-1">{moment(date).format("DD-MM-YYYY HH:mm:ss")}</div> */}
