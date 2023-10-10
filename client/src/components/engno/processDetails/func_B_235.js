13729d9d3f4fff6cbcb6b9d7cfb1260167670fdf
MOHAMMED YOUSUF <yousuf@tiei.toyota-industries.com>
2023-10-10 08:52:33 +0530

    null check confirmation for B OP 235

--- client/src/components/engno/processDetails/func_B_235.js
+++ client/src/components/engno/processDetails/func_B_235.js
@@ -2,14 +2,14 @@ export const decodeBlock235 = function (B3) {
   var result = [];
   // let B3 =
   //   "361112230706957021219D23F080000000071C23F180000000028DAC3EF80000000082BE3F180000000028FD83EEF00000000632D3F18000000002D0BD3EC20000000069C73F1800000000229D73F3C00000000400C3F1800000000269143F080000000059DF3F1800000000 00000000000000000000000000000000244E63F5E000000006C313F1800000000";
-  let D3 = B3.slice(0, 16);
+  let D3 = B3?.slice(0, 16);
   result.push(D3);
   var E3, G3, H3, I3, J3, K3, P3, L3, M3, N3, Q3;
 
   function vehicleTypeFn(c, d) {
-    if (B3.slice(c, d) == "2") {
+    if (B3?.slice(c, d) == "2") {
       return { model: "Module 2", lts: "1.5 Lts." };
-    } else if (B3.slice(c, d) == "3") {
+    } else if (B3?.slice(c, d) == "3") {
       return { model: "Module 3", lts: "2 Lts." };
     } else {
       return { model: "Element not found", lts: " " };
@@ -18,9 +18,9 @@ export const decodeBlock235 = function (B3) {
   E3 = vehicleTypeFn(16, 17);
   result.push(E3);
 
-  if (B3.slice(17, 18) == "1") {
+  if (B3?.slice(17, 18) == "1") {
     G3 = "OK";
-  } else if (B3.slice(17, 18) == "2") {
+  } else if (B3?.slice(17, 18) == "2") {
     G3 = "NG";
   } else {
     G3 = "element not found";
@@ -29,15 +29,15 @@ export const decodeBlock235 = function (B3) {
   result.push(G3);
   // console.log("-----------")
   // console.log("JUDGEMENT")
-  if (B3.slice(18, 19) == "1") {
-  } else if (B3.slice(18, 19) == "2") {
-  } else if (B3.slice(18, 19) == "4") {
-  } else if (B3.slice(18, 19) == "9") {
-  } else if (B3.slice(18, 19) == "C") {
-  } else if (B3.slice(18, 19) == "D") {
+  if (B3?.slice(18, 19) == "1") {
+  } else if (B3?.slice(18, 19) == "2") {
+  } else if (B3?.slice(18, 19) == "4") {
+  } else if (B3?.slice(18, 19) == "9") {
+  } else if (B3?.slice(18, 19) == "C") {
+  } else if (B3?.slice(18, 19) == "D") {
   }
 
-  H3 = B3.slice(23, 27) + B3.slice(19, 23);
+  H3 = B3?.slice(23, 27) + B3?.slice(19, 23);
 
   // =INT(HEX2DEC(LEFT(H3))/8)
 
@@ -48,7 +48,7 @@ export const decodeBlock235 = function (B3) {
 
   // =MOD(INT(HEX2DEC(LEFT(H3,3))/8),256)-127
 
-  const sampleJ3 = H3.slice(0, 3); // Get the leftmost 3 characters
+  const sampleJ3 = H3?.slice(0, 3); // Get the leftmost 3 characters
   const HEX2DECJ3 = parseInt(sampleJ3, 16); // Convert to decimal
   const PIJ3 = HEX2DECJ3 / 8; // Divide by 8
   const INTJ3 = Math.floor(PIJ3); // Take the integer part using Math.floor
@@ -57,7 +57,7 @@ export const decodeBlock235 = function (B3) {
   // This should output -1 for the given H3 value
   // =MOD(HEX2DEC(RIGHT(H3,6)),2^23)
 
-  K3 = parseInt(H3.slice(2, 8), 16) % Math.pow(2, 23);
+  K3 = parseInt(H3?.slice(2, 8), 16) % Math.pow(2, 23);
 
   // =(K3+2^23)*2^(J3-23)*-1^I3
 
@@ -79,7 +79,7 @@ export const decodeBlock235 = function (B3) {
 
   //  =IF(J3=128,N3,IF(J3=-127,IF(AND(I3=1,M3=0),"-0",M3),L3))
 
-  P3 = B3.slice(31, 35) + B3.slice(27, 30);
+  P3 = B3?.slice(31, 35) + B3?.slice(27, 30);
 
   const HEX2DECQ3 = parseInt(P3, 16) / 8;
   Q3 = parseInt(HEX2DECQ3);
@@ -91,7 +91,7 @@ export const decodeBlock235 = function (B3) {
     // function for checking judgement
     function judgement(a, b) {
       let value0;
-      let judg = B3.slice(a, b);
+      let judg = B3?.slice(a, b);
       if (judg === "1") {
         value0 = "LL_NG";
       } else if (judg === "2") {
@@ -121,14 +121,14 @@ export const decodeBlock235 = function (B3) {
 
     // let value1 = B3.slice(firstNumber+i+4, firstNumber+i+8) + B3.slice(firstNumber+i, firstNumber+i+4)
     let value1 =
-      B3.slice(firstNumber + i + 4, firstNumber + i + 8) +
-      B3.slice(firstNumber + i, firstNumber + i + 4);
+      B3?.slice(firstNumber + i + 4, firstNumber + i + 8) +
+      B3?.slice(firstNumber + i, firstNumber + i + 4);
     // INT(HEX2DEC(LEFT(H3))/8)
-    let value2 = parseInt(parseInt(value1.slice(0, 1), 16) / 8);
+    let value2 = parseInt(parseInt(value1?.slice(0, 1), 16) / 8);
 
-    let value3 = (parseInt(parseInt(value1.slice(0, 3), 16) / 8) % 256) - 127;
+    let value3 = (parseInt(parseInt(value1?.slice(0, 3), 16) / 8) % 256) - 127;
     // =MOD(HEX2DEC(RIGHT(H3,6)),2^23)
-    let value4 = parseInt(value1.slice(2, 8), 16) % Math.pow(2, 23);
+    let value4 = parseInt(value1?.slice(2, 8), 16) % Math.pow(2, 23);
     // =(K3+2^23)*2^(J3-23)*-1^I3
     let value5 =
       (value4 + Math.pow(2, 23)) *
