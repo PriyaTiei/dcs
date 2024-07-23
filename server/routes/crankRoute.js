42e5a817fc2aaae5f3be32fb58254888d383cc21
Sanjana <145703584+Sanjana1407a@users.noreply.github.com>
2024-07-23 15:05:57 +0530

    Add files via upload

--- server/routes/crankRoute.js
+++ server/routes/crankRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //everything being fetched from newControllers file in controllers folder
 const {
-    crankDisplay,getCrankinfoById, updateCrankinfoById,deleteCrankinfoById,createNewCrankinfo
+    crankDisplay,getCrankinfoById, updateCrankinfoById,deleteCrankinfoById,createNewCrankinfo,getCrankinfoByEngineNo,updateCrankinfoByEngineNo,deleteCrankinfoByEngineNo
 } = require("../controllers/newController");
 
 router.post("/crankinfo", createNewCrankinfo); //insert new document
@@ -11,5 +11,8 @@ router.get("/crankinfo", crankDisplay); //display all documents
 router.delete("/crankinfo/:id",deleteCrankinfoById);
 router.get("/crankinfo/:id",getCrankinfoById);
 router.put("/crankinfo/:id",updateCrankinfoById);
+router.get('/crankinformation/:engineNo', getCrankinfoByEngineNo);
+router.put("/crankinformation/:engineNo",updateCrankinfoByEngineNo);
+router.delete("/crankinformation/:engineNo",deleteCrankinfoByEngineNo);
 module.exports = router;
 
